8c446110b984dd2fbb4afbc01d72d5bf16098eaa
Davide Tamburrino <davide.tamburrino@gmail.com>
2024-09-03 16:40:05 +0200

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -300,7 +300,7 @@ class TestReporter {
         this.token = core.getInput('token', { required: true });
         this.context = (0, github_utils_1.getCheckRunContext)();
         this.octokit = github.getOctokit(this.token);
-        this.coverage = ore.getInput('coverage', { required: true });
+        this.coverage = core.getInput('coverage', { required: true });
         if (this.listSuites !== 'all' && this.listSuites !== 'failed') {
             core.setFailed(`Input parameter 'list-suites' has invalid value`);
             return;
